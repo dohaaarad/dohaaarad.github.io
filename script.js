e7d4be81d7ac43fc12dfbdc6b5bc455a5a30cc37
doha aarad <134413105+dohaaarad@users.noreply.github.com>
2023-05-28 00:28:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  } 
+
+
